ed7f35a9763964cbf6b6ae5738289d25236b01ed
Alfred Wutschka <hixels@gmx.de>
2023-02-03 12:00:34 +0100

    20230203

--- Main/src/json/MetadataContentScanner.java
+++ Main/src/json/MetadataContentScanner.java
@@ -15,11 +15,12 @@ import com.jayway.jsonpath.Option;
 
 public class MetadataContentScanner {
 	static File apiAntwortOrdner = new File(Drive.apiAntwortPfad);
-	
+
 	public static LinkedList<String> scanContent(String pfad) throws IOException {
+		System.out.println("MetadatenContenScan für '" + pfad + "'");
 		LinkedList<String> ret = new LinkedList<String>();
 		int max = 0;
-		for(File file: apiAntwortOrdner.listFiles()) {
+		for (File file : apiAntwortOrdner.listFiles()) {
 			--max;
 			if (max == 0) {
 				break;
@@ -27,12 +28,14 @@ public class MetadataContentScanner {
 			if (file.getName().startsWith(".")) {
 				continue;
 			}
-			String apiAntwortJson = Drive.loadFileToString(file);
-			JSONObject obj = new JSONObject(apiAntwortJson);
-			if (obj.has("parentPid")) {
-				continue;
-			}
-			DocumentContext json = JsonPath.parse(file, Configuration.defaultConfiguration().addOptions(Option.SUPPRESS_EXCEPTIONS, Option.ALWAYS_RETURN_LIST));
+//			String apiAntwortJson = Drive.loadFileToString(file);
+//			JSONObject obj = new JSONObject(apiAntwortJson);
+//			if (obj.getString("contentType").contentEquals("file")
+//					|| obj.getString("contentType").contentEquals("part")) {
+//				continue;
+//			}
+			DocumentContext json = JsonPath.parse(file, Configuration.defaultConfiguration()
+					.addOptions(Option.SUPPRESS_EXCEPTIONS, Option.ALWAYS_RETURN_LIST));
 //			System.out.println(json.read(pfad).toString());
 			net.minidev.json.JSONArray arr = json.read(pfad);
 //			if (arr. .containsKey("parentPid")) {
@@ -46,7 +49,7 @@ public class MetadataContentScanner {
 //				System.out.println(arr.get(i));
 			}
 		}
-		for (String str: ret) {
+		for (String str : ret) {
 			System.out.println(str);
 		}
 		return ret;
@@ -55,7 +58,11 @@ public class MetadataContentScanner {
 	public static void main(String[] args) throws IOException {
 //		LinkedList<String> ret = scanContent("$.contribution[*].role[*].label");
 //		LinkedList<String> ret = scanContent("$.license[*].@id");
-		LinkedList<String> ret = scanContent("$.license[*].note");
+//		LinkedList<String> ret = scanContent("$.license[*].note");
+//		LinkedList<String> ret = scanContent("$.accessScheme");
+//		LinkedList<String> ret = scanContent("$.publishScheme");
+//		LinkedList<String> ret = scanContent("$.hasData");
+		LinkedList<String> ret = scanContent("$.contentType");
 //		System.out.println(ret.toString()); nicht nötig, wird bereits ausgegeben.
 		System.out.println("MetadataContentScanner Ende...");
 	}
