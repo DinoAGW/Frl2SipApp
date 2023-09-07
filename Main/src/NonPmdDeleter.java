d0e2ee925496323ce9ce3543f3aa4dc14698bf35
Alfred Wutschka <hixels@gmx.de>
2023-09-07 14:29:47 +0200

    2023_09_07 commit

--- Main/src/NonPmdDeleter.java
+++ Main/src/NonPmdDeleter.java
@@ -6,15 +6,20 @@ import utilities.Drive;
 
 public class NonPmdDeleter {
 	static File apiAntwortOrdner = new File(Drive.apiAntwortPfad);
-	
+
 	public static void deleteAllPartsAndFiles () throws Exception {
 		for(File file: apiAntwortOrdner.listFiles()) {
 			if (file.getName().startsWith(".")) {
 				continue;
 			}
 			String apiAntwortJson = Drive.loadFileToString(file);
-			JSONObject obj = new JSONObject(apiAntwortJson);
-			if (obj.getString("contentType").contentEquals("file") || obj.getString("contentType").contentEquals("part")) {
+			JSONObject obj;
+			try {
+				obj = new JSONObject(apiAntwortJson);	
+				if (obj.getString("contentType").contentEquals("file") || obj.getString("contentType").contentEquals("part")) {
+				file.delete();
+				}
+			} catch (Exception e) {
 				file.delete();
 			}
 		}
