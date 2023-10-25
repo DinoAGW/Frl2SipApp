3814353acb65d7fafc31773ba55cc0de8b2dc8d6
Dino Wutschka <hixels@gmx.de>
2023-10-25 13:36:53 +0200

    2023_10_25 commit

---
+++ Main/src/tools/Json2JsonldRenamer.java
@@ -0,0 +1,34 @@
+package tools;
+
+import java.io.File;
+
+import utilities.Drive;
+
+public class Json2JsonldRenamer {
+	static File apiAntwortOrdner = new File(Drive.apiAntwortPfad);
+	
+	private static void scan() throws Exception {
+		int max = 0;
+		for (File file : apiAntwortOrdner.listFiles()) {
+			if (file.getName().startsWith(".")) {
+				continue;
+			}
+			if (file.getName().endsWith(".json")) {
+				File dest = new File(file.getAbsolutePath().concat("ld"));
+				System.out.println("Renaming " + file.getName() + " to " + dest.getName() + ".");
+				file.renameTo(dest);
+				--max;
+				if (max == 0) {
+					break;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		scan();
+		System.out.println("Json2JsonldRenamer Ende");
+
+	}
+
+}
