79182a1e382b10d583a5a1c0a82d0dc111056cbc
Dino Wutschka <hixels@gmx.de>
2023-11-27 16:53:35 +0100

    2023_11_27 commit

---
+++ Main/src/tools/PmdFindToResource.java
@@ -0,0 +1,46 @@
+package tools;
+
+import java.io.File;
+
+import org.json.JSONObject;
+
+import utilities.Drive;
+import utilities.Url;
+
+public class PmdFindToResource {
+	static File apiAntwortOrdner = new File(Drive.apiAntwortPfad);
+
+	public static void convertFindToResource() throws Exception {
+		for (File file : apiAntwortOrdner.listFiles()) {
+			if (file.getName().startsWith(".")) {
+				continue;
+			}
+			String apiAntwortJson = Drive.loadFileToString(file);
+			JSONObject obj = new JSONObject(apiAntwortJson);
+			if (obj.getString("contentType").contentEquals("file")
+					|| obj.getString("contentType").contentEquals("part")) {
+				continue;
+			}
+			String id = obj.optString("@id");
+			if (!id.startsWith("frl:")) {
+				throw new Exception("@id beginnt nicht mit 'frl:': '" + id + "'");
+			}
+			String url = "https://frl.publisso.de/resource/".concat(id).concat(".json2");
+			String stringApiAntwortJson = Url.getText(url);// versucht sich den Datensatz herunter zu laden
+			Thread.sleep(1000);
+			try {// speichert das aber nur ab, wenn es eine gültige Json ist. z.B. weil Datensatz
+					// private ist.
+				obj = new JSONObject(stringApiAntwortJson);
+			} catch (Exception e) {
+				System.err.println("Fehler beim Verarbeiten von id '" + id + "'. Antwort wird nicht gespeichert.");
+				continue;
+			}
+			Drive.saveStringToFile(obj.toString(2), Drive.apiAntwort(id.substring(4)));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		convertFindToResource();
+		System.out.println("PmdFindToResource Ende");
+	}
+}
